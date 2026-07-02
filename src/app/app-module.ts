b1116d1e721a683f2c5d4a553a5e13cd29514768
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2026-07-02 16:10:16 -0600

    Box model component

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { CardList } from './components/card-list/card-list';
+import { BoxModel } from './components/box-model/box-model';
 
 @NgModule({
-  declarations: [App, CardList],
+  declarations: [App, CardList, BoxModel],
   imports: [BrowserModule, AppRoutingModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
